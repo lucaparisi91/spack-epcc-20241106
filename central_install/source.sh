3548dc210aeac3e3e2834823027dda36f7cf676e
lucaparisi91 <luca.parisi91@gamil.com>
2024-11-27 17:05:51 +0000

    Restructured the config directory. Removed the central installation which is no longer required

--- central_install/source.sh
+++
@@ -1,3 +0,0 @@
-export SPACK_USER_CACHE_PATH=${SPACK_ROOT_EPCC}/.spack
-
-source $SPACK_ROOT/share/spack/setup-env.sh
--- user_install/source.sh
+++
@@ -1,3 +0,0 @@
-export SPACK_USER_CACHE_PATH=/work/$(id -gn)/$(id -gn)/$(id -un)/.spack
-
-source $SPACK_ROOT/share/spack/setup-env.sh
